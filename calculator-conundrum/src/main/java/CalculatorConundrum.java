62b5f0b69ac382683160417d724546c927c48cc3
Julián rodríguez Olaya <100172561+SHULIUS@users.noreply.github.com>
2025-04-14 19:36:03 -0500

    Implements basic calculator operations
    Adds support for addition, multiplication, and division operations in the calculator.
    
    Improves error handling for null, empty, and invalid operations, including division by zero.
    
    Enhances user experience by providing clear error messages.

--- calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,21 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+       try {
+            switch (operation) {
+                case "+": return operand1 + " + " + operand2 + " = " + (operand1 + operand2);
+                case "*": return operand1 + " * " + operand2 + " = " + (operand1 * operand2);
+                case "/": return operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+                case null: throw new IllegalArgumentException("Operation cannot be null");
+                case "": throw new IllegalArgumentException("Operation cannot be empty");
+                default : 
+                    throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            }
+        } catch (ArithmeticException e) {
+           throw new IllegalOperationException("Division by zero is not allowed", e);
+        }
     }
 }
+
+
+
+
